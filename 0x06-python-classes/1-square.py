111fac105365e4d5f4c8a94547c281681ccfb6ca
rayoo22 <ryankibet14@gmail.com>
2023-01-30 00:58:37 -0800

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Square Module """
+
+class Square:
+    """ Declaring the class """
+
+    def __init__(self, size):
+    """ defining a private instance """
+
+        self.__size = size
