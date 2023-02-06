a048b1d139aecd7f1214aff83d9cdd540c6c4500
rayoo22 <ryankibet14@gmail.com>
2023-02-05 23:57:18 -0800

    tasks

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" to check if object is of a specified class """
+
+def is_same_class(obj, a_class):
+    """ function to check if is of the class a_class """
+    return(isinstance(obj, a_class))
