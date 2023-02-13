cc3c9eb20b6aa42bb92e50c47c6e5fec04e1ec79
rayoo22 <ryankibet14@gmail.com>
2023-02-13 01:57:11 -0800

    tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Base Module """
+
+class Base:
+    """
+    __nb_objects: private class attribute
+    def __init__(self, id=None): class constructor
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize id, increment class attribute if no id is set as id """
+        if id:
+            self.id = id
+
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
