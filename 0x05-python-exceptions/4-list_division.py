b3b7838644d0a2f1666e84e173820195ccf97237
rayoo22 <ryankibet14@gmail.com>
2023-02-16 08:20:14 -0800

    tasks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def list_division(my_list, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     newList = []
     for i in range(list_length):
         div = 0
