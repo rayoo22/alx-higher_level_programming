31e78feebaa6865df23d0b25cd5f91786a7088bc
rayoo22 <ryankibet14@gmail.com>
2023-03-07 04:10:08 -0800

    test_driven_development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+def say_my_name(first_name, last_name=""):
+
+    if (isinstance(first_name, str)) == True:
+        if (isinstance(last_name, str)) == True:
+            print(f"My name is {first_name} {last_name}")
+    else:
+        print("
+
