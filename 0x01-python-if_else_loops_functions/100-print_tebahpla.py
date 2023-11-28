eda8e2399aa6e5103bab2cfc88d54e5c63680e2f
rayoo22 <ryankibet14@gmail.com>
2023-11-28 08:07:10 -0800

    prints the ASCII in lowercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for i in range(123, 96, -1):
+
+    """
     print("{}".format(chr(i)) if i % 2 == 0 else "{}".format(chr(i-32)), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-    print("{}".format(chr(ch)), end="")
-    if ch == 101 and ch == 113:
+    if ch in [101, 113]:
         continue
+    print("{}".format(chr(ch)), end='')
