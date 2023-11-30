55b82a38a4d0ba6a6ac0ed715125374e2efe3dca
rayoo22 <ryankibet14@gmail.com>
2023-11-30 10:06:56 -0800

    importing calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,4 +5,4 @@ b = 5
 print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
 print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
 print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+print("{} / {} = {}".format(a, b, int(calculator_1.div(a, b))))
