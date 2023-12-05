f160c719e41b9f778f2d412d0e257bfe014100df
rayoo22 <ryankibet14@gmail.com>
2023-12-04 16:57:49 -0800

    replaces element of list at idx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,5 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return
     else:
-        my_list[idx] = elemenit
+        my_list[idx] = element
         return my_list
