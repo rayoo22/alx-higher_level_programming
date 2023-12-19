7a40ea412fd04f51fb7fff54ff08b8568e4e24be
rayoo22 <ryankibet14@gmail.com>
2023-12-19 01:31:06 -0800

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """ defines a square by size """
+    def __init__(self, size):
+        """ private instance variable """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """ defines a square by size """
+    def __init__(self, size=0):
+        """ private instance variable """
+        self.__size = size
+
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
