e355dd093ec32f668fe5a230dfeb68246da5d5b4
rayoo22 <ryankibet14@gmail.com>
2024-01-04 02:19:27 -0800

    giving width and height attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
             height: height of rectangle object
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         
     @property
     def width(self):
