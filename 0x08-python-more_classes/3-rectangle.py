843327d8422b8f1b80c95ce54646878028d78184
rayoo22 <ryankibet14@gmail.com>
2024-01-04 04:12:47 -0800

    string representation of rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+""" The Rectangle Module
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Args:
+            width: width of rectangle object
+            height: height of rectangle object
+        """
+        self.__width = width
+        self.__height = height
+        
+    @property
+    def width(self):
+        """
+        property getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Args:
+            value: property setter
+        """
+        if isinstance(self.__width, int) is not True:
+            raise TypeError("width must be an integer")
+        elif self.__width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        property getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Args:
+           value: property setter
+        """
+        if isinstance(self.__height, int) is not True:
+            raise TypeError("height must be an integer")
+        elif self.__height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        Returns: area of rectangle
+        """
+        area = self.__width * self.__height
+        return area
+
+    def perimeter(self):
+        """
+        Returns the perimeter of rectangle object
+        """
+        if self.__width == 0 | self.__height == 0:
+            perimeter = 0
+        else:
+            perimeter = (self.__width + self.__height) * 2
+
+        return perimeter
+
+    def print_rectangle(self):
+        """ prints a rectangle """
+        for i in range(self.__width):
+            for j in range(self.__height):
+                print("#", end='')
+            print()
+
+    def __str__(self):
+        rec = print_rectangle()
+        return str(rec)
