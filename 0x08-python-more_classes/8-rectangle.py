8eec5908fc51a18f3516f544c277a50a017cdf38
rayoo22 <ryankibet14@gmail.com>
2024-01-04 14:02:48 -0800

    returns the biggest rectangle as per area

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -91,11 +91,11 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if isinstance(rect_2, Rectangle) is not True:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        elif rect_1.area() == rect_2.area():
-            return rect_1.area()
+        if rect_1.area() == rect_2.area():
+            return rect_1
 
         else:
             if rect_1.area() >= rect_2.area():
-                return rect_1.area()
+                return rect_1
             else:
-                return rect_2.area()
+                return rect_2
