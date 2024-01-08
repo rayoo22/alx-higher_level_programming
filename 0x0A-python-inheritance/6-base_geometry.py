92191aa9d213a7129bd8b9da2f1564d4ce97ff6a
rayoo22 <ryankibet14@gmail.com>
2024-01-08 06:11:17 -0800

    raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry"""
+
+
+class BaseGeometry:
+    """Has a public instnace method only"""
+    def area(self):
+        raise Exception("area() is not implemented")
