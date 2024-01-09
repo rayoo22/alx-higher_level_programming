3ad053f0da2bf736582c2ee65edaa0fd53c3fd96
rayoo22 <ryankibet14@gmail.com>
2024-01-09 04:17:50 -0800

    appends txt, counts chars of str added

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -13,4 +13,4 @@ def append_write(filename="", text=""):
     with open(filename, mode="a", encoding="utf-8") as myFile:
         myFile.write(text)
 
-        return len(text)            
+        return len(text)
