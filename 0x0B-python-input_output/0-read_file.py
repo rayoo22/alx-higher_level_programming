a1b08014cde10b40c59c16dc9c8f50a751487ca5
rayoo22 <ryankibet14@gmail.com>
2024-01-09 01:50:50 -0800

    reads text in file,prints to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     Args:
         filename: file to read from
     """
-    with open(filename, encoding='utf-8') as myFile:
-        print(myFile.read())
+    with open(filename, 'r', encoding='utf-8') as myFile:
+        print(myFile.read(), end="")
