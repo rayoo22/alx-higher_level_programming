1318758bf8b0a695983622e6b3c99e5a9788f4aa
rayoo22 <ryankibet14@gmail.com>
2024-01-09 04:42:59 -0800

    json representation of a string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -10,7 +10,4 @@ def to_json_string(my_obj):
     Returns:
         json representation of my_obj
     """
-    value = {
-            "my_obj": my_obj
-        }
-    return json.dumps(value)
+    return json.dumps(my_obj)
