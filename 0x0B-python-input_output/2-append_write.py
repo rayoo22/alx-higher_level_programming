5657e7237229d6d75fc266c2ce2dd02e4bcb5b5c
rayoo22 <ryankibet14@gmail.com>
2024-01-09 04:12:32 -0800

    appends txt, counts chars of str added

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module 2-append_write"""
+
+
+def append_write(filename="", text=""):
+    """appending string at the end of filename
+    Args:
+        filename: file to append string to
+        text: string to append at end of file
+    Returns:
+        number of characters added at end of file
+    """
+    with open(filename, mode="a", encoding="utf-8") as myFile:
+        myFile.write(text)
+        return len(text)
+            
