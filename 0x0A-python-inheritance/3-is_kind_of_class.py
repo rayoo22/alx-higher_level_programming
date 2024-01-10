a6f6f42d4f097e3a36933e6fc492be97e0762c61
rayoo22 <ryankibet14@gmail.com>
2024-01-10 05:05:29 -0800

    isinstance() function

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,14 +3,12 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """checks if obj is an instance of, or if obj is an 
-    instance of a class that inherited from
+    """checks if obj is an instance of a_class (direct/indirect)
     Args:
         obj: instace to check its derivation
         a_class: class or inherited class to check instance derivation
     Returns:
-        True if obj is an instnace of a_class or 
-        inherited class of a_class, otherwise False.
+        True if obj is an instance of a_class or otherwise False.
     """
     if isinstance(obj, a_class):
         return True
