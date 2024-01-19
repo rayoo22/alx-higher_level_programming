9994db9c04a8dbb1dfc1ba97d5e94a8c1e587c76
rayoo22 <ryankibet14@gmail.com>
2024-01-18 22:46:34 -0800

    repairing task 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,7 +97,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """string representation of the object created"""
-        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x,self.__y, self.__width, self.__height)
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, 
+                self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """uses args to print non-keyworded arguments and
