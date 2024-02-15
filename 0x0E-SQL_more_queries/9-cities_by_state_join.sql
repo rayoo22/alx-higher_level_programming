034ad340fc7ab7162d25ccc3cf881fe7ebaf9459
rayoo22 <ryankibet14@gmail.com>
2024-02-15 09:23:34 -0800

    lists cities in the database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,6 @@
 -- lists all cities contained in the database
-
+SELECT hbtn_0d_usa.cities.id, hbtn_0d_usa.cities.name, hbtn_0d_usa.states.name
+FROM hbtn_0d_usa.cities
+INNER JOIN hbtn_0d_usa.states
+ON hbtn_0d_usa.cities.state_id = hbtn_0d_usa.states.id
+ORDER BY hbtn_0d_usa.cities.id ASC;
