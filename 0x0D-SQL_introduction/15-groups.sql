e734774517d51e668de59ec1549fc58d453ab15d
rayoo22 <ryankibet14@gmail.com>
2024-03-05 08:38:45 -0800

    lists no. of records with same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score
 -- in the table second_table
-SELECT score, COUNT(*) AS number_of_records
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY number_of_records DESC;
+ORDER BY number DESC;
