570f2f8317e23e5114ce8536464b1c8eb441119d
rayoo22 <ryankibet14@gmail.com>
2024-03-07 07:00:47 -0800

    gets shows with atleast one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,9 @@
 -- lists all shows contained in hbtn_0d_tvshows
 -- that have atleast one genre lined
-SELECT DISTINCT hbtn_0d_tvshows.tv_shows.title, hbtn_0d_tvshows.tv_show_genres.genre_id
-FROM hbtn_0d_tvshows.tv_shows
-JOIN hbtn_0d_tvshows.tv_show_genre
-ON hbtn_0d_tvshows.tv_show.id = hbtn_0d_tvshows.tv_show_genre.show_id
-ORDER BY hbtn_0d_tvshows.tv_shows.title, hbtn_0d_tvshows.tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_shows.title, tv_show_genres.genre_id
+HAVING COUNT(tv_show_genres.genre_id) >= 1
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
