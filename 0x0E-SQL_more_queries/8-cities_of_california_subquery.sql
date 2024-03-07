eda694659d64a334d0690a4926908cd6cd5e5a45
rayoo22 <ryankibet14@gmail.com>
2024-03-07 05:38:30 -0800

    lists all cities of state=california, without JOIN keyword

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,10 @@
 -- lists all cities of CALIFORNIA STATE that can be found
 -- in hbtn_0d_usa databsase
-SELECT hbtn_0d_usa.cities.id, hbtn_0d_usa.cities.name
-FROM hbtn_0d_usa.cities
-WHERE hbtn_0d_usa.cities.state_id = (
-	SELECT hbtn_0d_usa.states.id
-	FROM hbtn_0d_usa.states
-	WHERE hbtn_0d_usa.states.name='California'
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+	SELECT states.id
+	FROM states
+	WHERE states.name='California'
 )
-ORDER BY hbtn_0d_usa.cities.id ASC;
+ORDER BY cities.id ASC;
