10b665950d974781e2a2db984745157a99859581
rayoo22 <ryankibet14@gmail.com>
2024-03-11 16:24:53 -0700

    prints "C is fun" n times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let val = parseInt(process.argv[2]);
+
+if (!isNaN(val))
+{
+	for (let i = 0; i < val; i++)
+	{
+		console.log('C is fun');
+	}
+}
+else
+{
+	console.log('Missing number of occurrences')
+}
