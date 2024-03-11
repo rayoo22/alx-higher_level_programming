ebef1a8ce48cfe49ec2ace744677f63478dc7adf
rayoo22 <ryankibet14@gmail.com>
2024-03-11 13:04:00 -0700

    prints three lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//script printing “C is fun”, “Python is cool”, “JavaScript is amazing”
+//script “C is fun”, “Python is cool”, “JavaScript is amazing”
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
