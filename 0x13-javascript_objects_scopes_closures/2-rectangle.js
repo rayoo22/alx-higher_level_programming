a8e0c15c1dbfd38280224fde37d9f34be3ddebe4
rayoo22 <ryankibet14@gmail.com>
2024-03-12 06:39:55 -0700

    checks if w or h are positive integers

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,10 @@
 // creates rectangle clas, with instance attributes
 // If w or h is equal to 0 or not a positive integer, create an empty object
 module.exports = class Rectangle {
-	constructor(w, h){
-		if (w > 0 && h > 0)
-		{
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 };
