0c206f9af0b9d25c1d2a699202750354cff182ca
rayoo22 <ryankibet14@gmail.com>
2024-03-12 06:57:41 -0700

    prints a rectangle using "X" character

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,26 +1,21 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-	constructor(w, h)
-	{
-		if (w > 0 && h > 0)
-		{
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print()
-	{
-		for (let i = 0; i < this.height; i++)
-		{
-			let x = "";
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
 
-			for (let j = 0; j < this.width; j++)
-			{
-				x += 'X';
-			}
-			console.log(x);
-		}
-	}
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
 };
