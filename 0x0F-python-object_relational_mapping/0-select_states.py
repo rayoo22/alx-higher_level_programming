da365a85bf2d8831ca4b99c52dba43417feb4eda
rayoo22 <ryankibet14@gmail.com>
2024-03-19 04:08:18 -0700

    lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 
-import MySQLdb
-import sys
 """ script that lists all states from the database hbtn_0e_0_usa
 takes 3 arguments mysql username, mysql password and database name
 sorts results in ascending order by states.id
 """
+import MySQLdb
+import sys
+
 try:
     db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
@@ -16,6 +17,8 @@ try:
 
     for state in list_of_states:
         print(state)
+    cur.close()
+    db.close()
 
 except MySQLdb.Error as e:
     print("Error:", e)
