2ce810b58d07270a00fab578328bc1b48797844b
rayoo22 <ryankibet14@gmail.com>
2024-03-20 01:05:36 -0700

    a python class "State" linking to table states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 
-import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
@@ -12,10 +10,3 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
-    # creates an engine to connect to the database
-    engine = create_engine(f'msqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
-
-    # Create tables in the database
-    Base.metadata.create_all(engine)
