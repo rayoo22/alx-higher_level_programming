bfe44078e38848321060a19f2b3ceee4b03faa5f
rayoo22 <ryankibet14@gmail.com>
2024-03-27 02:43:39 -0700

    gets url, sends URL request, displays response size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # takes in URL, send s request to URL, displays size of
 # body of the response
-response_size=$(curl -s "$1" | wc -c)
-echo "$response_size"
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
