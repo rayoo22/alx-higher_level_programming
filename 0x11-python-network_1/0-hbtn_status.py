d958779455157c36b7174c62e41967abd380130e
rayoo22 <ryankibet14@gmail.com>
2024-03-28 05:43:56 -0700

    fetches url https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,6 @@
 import urllib.request
 
 if __name__ == "__main__":
-    """
-    fetches 'https://alx-intranet.hbtn.io/status
-    """
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
