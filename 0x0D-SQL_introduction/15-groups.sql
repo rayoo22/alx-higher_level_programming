df467d2e758fc0626aca9a1e68822acfa2e48c02
ryan kibet <ryankibet14@gmail.com>
2024-05-21 20:33:48 +0300

    lists number of records with same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,2 @@
 -- lists number of records with same score
-SELECT score, COUNT(*) AS number
-FROM second_table 
-GROUP BY score 
-HAVING number > 1 
-ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score HAVING number > 1 ORDER BY score DESC;
