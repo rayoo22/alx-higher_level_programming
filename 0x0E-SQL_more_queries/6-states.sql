ef745703360a79da2539ddb1482bb8c5ffa8cbcc
ryan kibet <ryankibet14@gmail.com>
2024-05-22 12:36:44 +0300

    reates db hbtn_0d_usa, and table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates db hbtn_0d_usa, and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+	name VARCHAR(256) NOT NULL
+	);
