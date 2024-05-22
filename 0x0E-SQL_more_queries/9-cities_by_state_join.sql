811353c6ad114f0ac12603403142ad2f30fb27c9
ryan kibet <ryankibet14@gmail.com>
2024-05-22 19:22:37 +0300

    lists all cities of california without using the JOIN KEYWORD

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities of california without using the JOIN KEYWORD
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities INNER JOIN hbtn_0d_usa.states ON cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
