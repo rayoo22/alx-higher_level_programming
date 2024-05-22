df9885ac706025bc81b50c6af3bbed88e4602d7a
ryan kibet <ryankibet14@gmail.com>
2024-05-23 01:51:36 +0300

    lists all genres displayin number of shows linked to them

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- lists all genres displayin number of shows linked to them
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id WHERE tv_show_genres.show_id IS NOT NULL GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
