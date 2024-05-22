cb7cddcc07f33433c40170f1630ae560217db6e3
ryan kibet <ryankibet14@gmail.com>
2024-05-22 18:35:17 +0300

    lists all cities of california without using the JOIN KEYWORD

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,2 @@
 -- lists all cities of california without using the JOIN KEYWORD
-SELECT id, name
-FROM hbtn_0d_usa.cities
-WHERE id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California';)
-ORDER BY hbtn_0d_usa.cities.id ASC
+SELECT id, name FROM hbtn_0d_usa.cities WHERE id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY hbtn_0d_usa.cities.id ASC;
