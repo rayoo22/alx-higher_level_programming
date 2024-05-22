7864cddc6e2d78bf137e9f2b217e4ac8d23db0e1
ryan kibet <ryankibet14@gmail.com>
2024-05-22 09:38:40 +0300

    create user user_0d_1 with all privileges, and a password set

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create user user_0d_1 with all privileges, and a password set
+CREATE USER 'user_0d_1' IDENTIFIED WITH 'caching_sha2_password' BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
