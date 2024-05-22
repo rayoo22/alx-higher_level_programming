8a34bb801d2228a063959d526642b5fa4ab824e3
ryan kibet <ryankibet14@gmail.com>
2024-05-22 19:20:59 +0300

    lists all shows hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- lists all shows hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows.tv_shows RIGHT JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
