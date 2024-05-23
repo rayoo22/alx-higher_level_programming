97cd4090712a1ce1cadd7fdf03aa260d061e587e
ryan kibet <ryankibet14@gmail.com>
2024-05-23 12:19:57 +0300

    lists all genres of show Dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- lists all genres of show Dexter
-SELECT tv_genres.name FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id WHERE tv_show_genres.show_id=8 ORDER BY tv_genres.name ASC;
+SELECT tv_genres.name FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id WHERE tv_shows.title='Dexter' ORDER BY tv_genres.name ASC;
