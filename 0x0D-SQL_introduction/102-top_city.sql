92af8e4603327403a7d93e21157c15966946359d
ryan kibet <ryankibet14@gmail.com>
2024-05-23 17:21:53 +0300

    displays top 3 cities

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
--- displays top 3 cities 
-SELECT temperatures.city, AVG(temperatures.value) AS avg_temp FROM temperatures GROUP BY temperatures.city ORDER BY avg_temp DESC LIMIT 3;
+-- displays top 3 cities
+SELECT temperatures.city, AVG(temperatures.value) AS avg_temp FROM temperatures WHERE temperatures.month in (7, 8) GROUP BY temperatures.city ORDER BY avg_temp DESC LIMIT 3;
