a61128561a7998264762c0fef23270f8ee25f7fe
ryan kibet <ryankibet14@gmail.com>
2024-05-23 17:22:58 +0300

    displays top 3 cities between July and August

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
--- displays top 3 cities
+-- displays top 3 cities between July and August
 SELECT temperatures.city, AVG(temperatures.value) AS avg_temp FROM temperatures WHERE temperatures.month in (7, 8) GROUP BY temperatures.city ORDER BY avg_temp DESC LIMIT 3;
