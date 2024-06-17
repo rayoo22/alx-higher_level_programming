e8f6b5c582aac1e0b196497a3735271df9da4672
ryan kibet <ryankibet14@gmail.com>
2024-06-17 23:36:57 +0300

    prints C is fun as per arg passed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-let x = process.argv[2]
+const x = process.argv[2];
 
 if (!parseInt(process.argv[2])) {
-	console.log('Missing number of occurrences');
-}
-else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
