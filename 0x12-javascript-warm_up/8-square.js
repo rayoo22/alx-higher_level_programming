b74db75809b4cd09705a63a9ff6bd18644a22a6f
ryan kibet <ryankibet14@gmail.com>
2024-06-17 23:51:24 +0300

    prints square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const size = process.argv[2];
+
+if (!parseInt(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+	  let line = '';
+	  for (let j = 0; j < size; j++) {
+      		line += 'X';
+	  }
+	  console.log(line);
+  }
+}
