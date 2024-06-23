dd558b40677c4f6976de82a561ae2bc6d6c9e21f
rayoo22 <ryankibet14@gmail.com>
2024-06-23 02:27:37 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 # listing all records in the states table
 
-import sys
-import MySQLdb
 
 if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
@@ -14,6 +16,3 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    cur.close()
-    db.close()
