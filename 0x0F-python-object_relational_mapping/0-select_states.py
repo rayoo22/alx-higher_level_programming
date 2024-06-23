e4a56c6293cfec498e163ce60aa0d4f23029f073
ryan kibet <ryankibet14@gmail.com>
2024-06-23 19:50:22 +0300

    lists all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,12 @@
 # listing all records in the states table
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
 
@@ -16,6 +16,3 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-cur.close()
-db.close()
