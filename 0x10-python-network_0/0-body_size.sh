a8e76650a6e809e8489f9cede123b03a15658160
rayoo22 <ryankibet14@gmail.com>
2024-07-04 02:53:35 -0700

    takes in url, sends request to URL, displays size of body of response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
-# takes in url and sends request to that url, displaying the size
-# of the body of the response
-curl -s "$1" -o /dev/null -w "%{size_download}\n"
+# takes in url, sends request to URL, displays size of body of response
+curl -s "$1" | wc -c
