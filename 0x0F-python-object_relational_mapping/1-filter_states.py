522d5fcd7b390d62b3e8addcbb9378995a5122ad
rayoo22 <ryankibet14@gmail.com>
2024-07-18 08:40:29 -0700

    script that lists all states with a name starting with N (upper N)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,9 @@ if __name__ == '__main__':
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC;")
+    cur.execute("SELECT * FROM states
+            WHERE states.name LIKE 'N%'
+            ORDER BY states.id ASC;")
 
     rows = cur.fetchall()
     for row in rows:
