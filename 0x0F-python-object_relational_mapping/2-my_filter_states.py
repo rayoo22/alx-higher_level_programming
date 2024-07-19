6ad776b483ab4368ca83a5ce123dbdedb8b590fc
rayoo22 <ryankibet14@gmail.com>
2024-07-18 20:29:24 -0700

    searches for a state given as a argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-# listing all records in the states table
+""" listing all records in the states table """
 
-import MySQLdb
-import sys
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
 
     cur = db.cursor()
-    
-    state_name = sys.argv[4]
-    query = "SELECT * FROM states WHERE states.name = '{}'  ORDER BY states.id ASC".format(state_name)
+
+    statename = sys.argv[4]
+
+    query = "SELECT * FROM states WHERE states.name = '{}' ORDER BY states.id ASC;".format(statename)
 
     cur.execute(query)
 
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    cur.close()
-    db.close()
